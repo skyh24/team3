0f0e6b33eb8b72b3765a451b4e9417081924e459
skyh24 <skyhighfeng@gmail.com>
2020-06-10 17:19:28 +0800

    lesson 2

--- lesson2/substrate-node-template/runtime/src/lib.rs
+++ lesson2/substrate-node-template/runtime/src/lib.rs
@@ -42,6 +42,7 @@ pub use frame_support::{
 
 /// Importing a template pallet
 pub use template;
+pub use poe;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -256,6 +257,9 @@ impl sudo::Trait for Runtime {
 impl template::Trait for Runtime {
 	type Event = Event;
 }
+impl poe::Trait for Runtime {
+	type Event = Event;
+}
 
 construct_runtime!(
 	pub enum Runtime where
@@ -273,6 +277,7 @@ construct_runtime!(
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		PoeModule: poe::{Module, Call, Storage, Event<T>},
 	}
 );
 
